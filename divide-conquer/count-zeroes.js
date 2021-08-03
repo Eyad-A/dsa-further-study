c6d498eb49e0eac140b34715329c16943164ac6e
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-08-03 04:02:39 -0500

    Count Zeros

--- divide-conquer/count-zeroes.js
+++ divide-conquer/count-zeroes.js
@@ -1,5 +1,26 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let firstZero = findFirstZero(arr);
+  if (firstZero === -1) return 0;
+  return arr.length - firstZero;
+}
+
+function findFirstZero(arr, start = 0, end = arr.length - 1) {
+  if (end >= start) {
+    let mid = start + Math.floor((end - start) / 2);
+    if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+      return mid;
+    } else if (arr[mid] === 1) {
+      return findFirstZero(arr, mid + 1, end);
+    }
+    return findFirstZero(arr, start, mid - 1);
+  }
+  return -1;
 }
 
 module.exports = countZeroes
+
+
+// countZeroes([1, 1, 1, 1, 0, 0]) // 2
+// countZeroes([1, 0, 0, 0, 0]) // 4
+// countZeroes([0, 0, 0]) // 3
+// countZeroes([1, 1, 1, 1]) // 0
