d546a4f3eed75449999dbefe8b1df25acb3a2f20
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-08-10 02:11:16 -0500

    missing number

---
+++ private/find-peak.js
@@ -0,0 +1,27 @@
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[mid+1]) {
+            right = mid;
+        } else {
+            left = mid+1;
+        }
+    }
+    return left;    
+};
+
+// A peak element is an element that is strictly greater than its neighbors.
+// Given an integer array nums, find a peak element, and return its index. 
+// If the array contains multiple peaks, return the index to any of the peaks. 
+
+// Input: nums = [1,2,3,1]
+// Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2. 
+
+// Input: nums = [1,2,1,3,5,6,4]
+// Output: 5
+// Explanation: Your function can return either index number 1 where the peak element is 2, 
+// or index number 5 where the peak element is 6.
